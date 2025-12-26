66b83020b024a738e2b41875b66fec9b691652e3
calbor-p <calbor-p@ls>
2025-12-26 11:30:01 +0100

    new dump

--- srcs/requirements/mysql/dump/make-dump.sh
+++ srcs/requirements/mysql/dump/make-dump.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+set -a  # Exporte automatiquement les variables
+source ../../../.env
+set +a
+
 docker exec mysql mysqldump --all-databases --triggers --routines --events --single-transaction -uroot -p${MYSQL_ROOT_PASSWORD} > dump.sql
 git add --all
 git commit -m "new dump"
